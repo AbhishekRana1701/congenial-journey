0d8e901fecbdbdfdd0c85e7a911e8d2dd7f0829c
AbhishekRana1701 <ranabhishek962@gmail.com>
2025-02-17 21:57:47 +0530

    Add files via upload

---
+++ ArrayListEx.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+
+public class ArrayListEx {
+    public static void main(String[] args) {
+        ArrayList<String> friends = new ArrayList<String>();
+        friends.add("Bunty");
+        friends.add("Grumpy");
+        friends.add("Hunty");
+        friends.add("Dunty");
+        friends.add("Chunty");
+
+        System.out.println(friends);
+    }    
+}
