ff359a38de8e37d7858864c1d86e491d51534a60
AbhishekRana1701 <ranabhishek962@gmail.com>
2025-02-24 06:28:36 +0530

    Add files via upload

---
+++ IfStatement.java
@@ -0,0 +1,12 @@
+public class IfStatement{
+    public static void main(String[] args){
+        String str = "Babumoshai is Here";
+        int i = 5;
+        if (i == 5){
+            i++;
+            System.out.println(str);
+
+            System.out.println("i = " + i);
+        }
+    }
+}
